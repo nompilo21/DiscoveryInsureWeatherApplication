cd50b427d6500cb1698c58ec7aa22c443cf58d3b
Nompilo Angela Moyo <nompilomoyo11@gmail.com>
2023-09-03 22:05:28 +0200

    Add project files.

---
+++ DiscoverWeatherApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscoverWeatherApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DiscoverWeatherApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscoverWeatherApplication
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
